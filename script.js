19c6956f6d5a8ec52e473694032294341c9ae3e1
JosueMaximo <josuemaximooficial@gmail.com>
2023-12-26 22:00:15 -0300

    outra forma de fazer o 'for'

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ var p2 = document.querySelectorAll('.paragrafo');
 // O .length significa o comprimento/quantidade que possui esse array, então é preciso
 // colocar a variável com ele
 //o p[i] é essencial, pois ali ele vai estar modificando o style da var p2 na posição de [i]
-for(var i=0; i<p2.length; i++){
-    p2[i].style = "color: red";
+// for(var i=0; i<p2.length; i++){
+//     p2[i].style = "color: red";
+// }
+
+for(var i in p2){
+    p2[i].style = "color: green";
 }
